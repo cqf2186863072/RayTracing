ed64e04b567207405340ac271bfd8412d5bd6c95
GitHub cqf2186863072 <GitHub 2186863072@qq.com>
2024-08-19 01:32:03 +0800

    update

--- camera.h
+++ camera.h
@@ -3,6 +3,7 @@
 
 #include "headers.h"
 #include "hittable.h"
+#include "material.h"
 #include <fstream>
 
 class camera {
@@ -10,6 +11,7 @@ public:
     double  aspect_ratio        = 1.0;
     int     image_width         = 100;
     int     samples_per_pixel   = 10;
+    int     max_depth           = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -25,7 +27,7 @@ public:
                 color pixel_color(0, 0, 0);
                 for (int sample = 0; sample < samples_per_pixel; sample++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(myfile, pixel_color * pixel_samples_scale);
             }
@@ -87,11 +89,20 @@ private:
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        if (depth <= 0) {
+            return color(0, 0, 0);
+        }
+
         hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec)) {
-            vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+                return attenuation * ray_color(scattered, depth - 1, world);
+            }
+            return color(0, 0, 0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
